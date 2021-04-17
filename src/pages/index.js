bd46f3583683a9dcfed31c6335d09ffd250c505e
kotyan0427 <koulotte0427@outlook.jp>
2021-04-17 23:00:19 +0900

    ホームページとヘッダーページの作成

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Header } from "./../components/molecules/Header";
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>とある仙台住みのロッテブログ</title>
+      </Head>
+      <Header />
+    </>
+  );
+};
+
+export default Index;

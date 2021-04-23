6dae18dd36518b1b2a08d641c7fd2e528ed452e9
kotyan0427 <koulotte0427@outlook.jp>
2021-04-23 17:01:31 +0900

    Drawerコンポーネントの削除

--- src/components/organism/Header.jsx
+++ src/components/organism/Header.jsx
@@ -1,9 +1,4 @@
-import { useDisclosure } from "@chakra-ui/react";
-import { ResponsiveIcon } from "../atoms/icon/HamburgerIcon";
-import { DrawerTags } from "../molecules/Drawer";
-
 export const Header = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <header className="maxContainer h-24 ">
@@ -13,10 +8,8 @@ export const Header = () => {
             alt="ロゴ画像"
             className="w-60 h-12 object-cover bg-gray-200 rounded-lg"
           />
-          <ResponsiveIcon onOpen={onOpen} display={{ base: "block", md: "none" }} height={45} />
         </div>
       </header>
-      <DrawerTags isOpen={isOpen} onClose={onClose} />
     </>
   );
 };

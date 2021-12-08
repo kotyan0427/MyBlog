03f9ac0aaf9771429bbd48d3dc0bc48003ca079f
kotyan0427 <koulotte0427@outlook.jp>
2021-12-08 20:51:20 +0900

    recoilを導入してinputのvalue値を管理するstateの作成

--- src/components/atoms/input/SearchInput.tsx
+++ src/components/atoms/input/SearchInput.tsx
@@ -1,7 +1,20 @@
+import { useCallback } from "react";
+import { useRecoilState } from "recoil";
+import {searchValue} from "../../stores/searchValue"
+
 export const SearchInput = () => {
+  const [value, setValue] = useRecoilState(searchValue);
+
+  // inputのvalue値を取得する関数
+  const onChangeValue = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <input
       type="text"
+      value={value}
+      onChange={(e) => onChangeValue(e)}
       className="w-7/12 max-w-500 border py-1 px-2 focus:outline-none"
     />
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { RecoilRoot } from "recoil";
 import { AppProps } from 'next/app';
 import '../styles/tailwind.css';
 import theme from '../styles/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
   );
 };
 

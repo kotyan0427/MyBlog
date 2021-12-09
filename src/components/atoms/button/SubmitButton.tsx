4265f361ece1ff379a6769413a44d810dab15442
kotyan0427 <koulotte0427@outlook.jp>
2021-12-09 10:34:55 +0900

    検索ボタンコンポーネントの作成

--- src/components/atoms/button/SubmitButton.tsx
+++ src/components/atoms/button/SubmitButton.tsx
@@ -1,3 +1,11 @@
-export const SubmitButton = () => {
-  return <button className='border px-3 py-1 ml-2 bg-white duration-300 hover:bg-black hover:text-white'>SEARCH</button>
-}
+import React, { VFC } from 'react';
+
+export const SubmitButton: VFC = () => {
+  return (
+    <button
+      className="border px-3 py-1 ml-2 bg-white duration-300 hover:bg-black hover:text-white"
+    >
+      SEARCH
+    </button>
+  );
+};

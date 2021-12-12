cbf1cc14288c79d809df233850f40d710ddd1f5e
kotyan0427 <koulotte0427@outlook.jp>
2021-12-12 17:20:04 +0900

    ヘッダーにトップページに戻るリンクを貼る

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -1,15 +1,20 @@
 import { VFC } from 'react';
+import Link from "next/link";
 
 export const Header: VFC = () => {
   return (
     <>
       <header className="maxContainer h-16">
         <div className="flex justify-around items-center mt-1 md:text-center md:flex">
-          <img
-            src="/images/logoName.png"
-            alt="ロゴ画像"
-            className="w-60 h-12 object-cover bg-gray-200 rounded-lg"
-          />
+          <Link href="/">
+            <a>
+              <img
+                src="/images/logoName.png"
+                alt="ロゴ画像"
+                className="w-60 h-12 object-cover bg-gray-200 rounded-lg"
+              />
+            </a>
+          </Link>
         </div>
       </header>
     </>
